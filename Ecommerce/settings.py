569258130806f6485bb1fca10a12898826b0c887
Prateek Yadav <prtkdv2@gmail.com>
2023-03-06 23:56:08 +0530

    hided useful data

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(c#3h-1eds2l!vndwvf%px!4tegw0i1jkhduz^23eg%@%@wm#c'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from products import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('favourite', views.favourite, name="favourite")
 ]
