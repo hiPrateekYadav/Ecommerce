11b125543acb42bd04a5c75a6d25a22b7a2befd2
Prateek Yadav <prtkdv2@gmail.com>
2023-03-11 15:30:09 +0530

    added pre-commit-config file, made a folder structure for html/css/scss/js; added templates and static files

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from products import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('favourite', views.favourite, name="favourite")
+    path("", views.index, name="index"),
+    path("favourite", views.favourite, name="favourite"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import HttpResponse
+from django.shortcuts import render
+
 
 # Create your views here.
-def index(request) :
+def index(request):
     return HttpResponse("Home page")
 
-def favourite(request) :
-    return HttpResponse("fovourite page")
+
+def favourite(request):
+    return render(request, "home.html")
